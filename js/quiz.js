a12efe9b81961a2180f9c6f22ab531c464b137ba
Kaiser <kaiser@MacBook-Pro-som-tillhor-Kaiser.local>
2022-02-05 20:43:38 +0100

    js quize a ready

---
+++ js/quiz.js
@@ -0,0 +1,101 @@
+let quiz = [
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        option: [
+            "The head section",
+            "Both the head section and the body section are correct",
+            "The body section",
+            "None of the above",
+        ],
+        answer: 2,
+    },
+    {
+        question: "How many ways are there with which we can declare a variable in javascript?",
+        option: [
+            "Only one",
+            "Infinity many",
+            "Three",
+            "None of the above",
+        ],
+        answer: 3,
+    },
+    {
+        question: "What is the type of variable data declare below const data=[]?",
+        option: [
+            "Object",
+            "String",
+            "Array",
+            "None of the above",
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            "Which of the following javascript functions allows you to get user input data?",
+        option: [
+            "prompt",
+            "alert",
+            "confirm",
+            "None of the above",
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            "Which of the following represents loops in javascript?",
+        option: [
+            "for",
+            "while",
+            "forEach",
+            "All of the above",
+        ],
+        answer: 4,
+    },
+    {
+        question: "What scripting language is jQuery written in ",
+        option: [
+            "C++",
+            "VBScript",
+            "C#",
+            "javaScript"],
+        answer: 4,
+    },
+    {
+        question: "jQuery method is used to perform an asynchronous HTTP request?",
+        option: [
+            "jQuery.ajaxSetup",
+            "jQuery.ajaxAsync",
+            "jQuery.ajax",
+            "None of the above",
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            "What is negative infinity in Java script?",
+        option: [
+            "Any of below",
+            "number in JavaScript, derived by dividing number by a negative number ",
+            "number in JavaScript, derived by dividing number by zero",
+            "number in JavaScript, derived by dividing negative number by zero"],
+        answer: 4,
+    },
+    {
+        question: "Which function is a synonym for on()", 
+        option: [
+            "add()",
+            "once()",
+            "listeners()",
+            "addListener()"],
+        answer: 4,
+    },
+    {
+        question: "Which of the following is used for parsing JSON text?",
+        option: [
+            "jQuery.each()",
+            "jQuery.NOcONFLICT()",
+            "jQuery.parseJSON()",
+            "None of the above"],
+        answer: 3,
+    },
+];
